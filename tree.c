b58618b1c827e48c1a4714ad943f3b5702a7a59a
erik <erikzubi@ucm.es>
2024-10-06 11:56:51 +0200

    finished binary tree impl

--- tree.c
+++ tree.c
@@ -10,9 +10,112 @@ Tree root = { .n = {
     .path = "/"
 }};
 
+void zero(int8 *str, int16 size) {
+    int8 *p;
+    int16 n;
+
+    for(n=0, p=str; n<size; ++p, ++n) {
+        *p = 0;
+    }
+    return;
+}
+
+Node *create_node(Node *parent, int8 *path) {
+    Node *n;
+    int16 size;
+
+    assert(parent);
+    size = sizeof(struct s_node);
+    n = (Node *)malloc((int)size);
+    zero((int8 *)n, size);
+
+    parent->west = n;
+    n->tag = TagNode;
+    n->north = parent;
+    strncpy((char*)n->path, (char *)path, 255);
+
+    return n;
+}
+
+Leaf *find_last_linear(Node *parent) {
+    Leaf *l;
+
+    errno = NoError;
+    assert(parent);
+
+    if(!parent->east) {
+        reterr(NoError);
+    }
+    for(l=parent->east; l->east; l=l->east);
+    assert(l);
+
+    return l;
+}
+
+Leaf *create_leaf(Node *parent, int8 *key, int8 *value, int16 count) {
+    Leaf *l, *new;
+    int16 size;
+
+    assert(parent);
+    l = find_last(parent);
+
+    size =sizeof(struct s_leaf);
+    new = (Leaf *)malloc(size);
+    assert(new);
+
+    zero((int8 *)new, size);
+    new->tag = TagLeaf;
+
+    if(!l) { // directly connected, no hanging leaves
+        parent->east = new;
+        new->west = (Tree *)parent;
+    }
+    else { // l is a leaf
+        l->east = new; 
+        new->west = (Tree *)l;
+    }
+
+    strncpy((char *)new->key, (char *)key, 127);
+    new->value = (int8 *)malloc(count);
+    zero(new->value, count);
+    assert(new->value);
+    strncpy((char *)new->value, (char *)value, count);
+    new->size = count;
+
+    return new;
+}
+
 int main() {
+    Node *n, *n2;
+    Leaf *l1, *l2;
+    int8 *key, *value;
+    int16 size;
 
-    printf("%p\n", (void*)&root);
+    n = create_node((Node *)&root, (int8 *)"/Users"); 
+    assert(n);
+    n2 = create_node(n, (int8 *)"Users/login");
+    assert(n2);
+    
+    key = (int8 *)"erik";
+    value = (int8 *)"abc77301aa";
+    size = (int16)strlen((char *)value);
+    l1 = create_leaf(n2, key, value, size);
+    assert(l1);
 
+    printf("%s\n", l1->value);
+
+    key = (int8 *)"elena";
+    value = (int8 *)"aa034945c";
+    size = (int16)strlen((char *)value);
+    l2 = create_leaf(n2, key, value, size);
+    assert(l2);
+
+    printf("%s\n", l2->key);
+    
+    free(l2);
+    free(l1);
+    free(n2);
+    free(n);
+ 
     return 0;
 }
--- tree.h
+++ tree.h
@@ -13,6 +13,16 @@
 #define TagNode 2 /* 00 10 */
 #define TagLeaf 4 /* 01 00 */
 
+#define NoError 0
+
+//typedef void* Nullptr;
+//Nullptr nullptr = 0;
+
+#define find_last(x)    find_last_linear(x) // this way when changing the find function, we only need to change this statement instead of everywhere where it's called
+#define reterr(x) \
+    errno = (x); \
+    return nullptr
+
 typedef unsigned int int32;
 typedef unsigned short int int16;
 typedef unsigned char int8;
@@ -32,7 +42,7 @@ struct s_leaf {
     Tag tag;
     union u_tree *west; // either a node or a leaf
     struct s_leaf *east;
-    int8 key[128]; // each leaf has a key that distiguishes it
+    int8 key[128]; // each leaf has a key that distiguishes it, the path is included in the key
     int8 *value;
     int16 size;
 };
